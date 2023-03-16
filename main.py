eedfe61ac94e02a57d1956ae45876d682f79e34d
Carlos Dominguez <carlosdominguez77@gmail.com>
2023-03-16 22:52:37 +0100

    Add layout, content and images

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas
 st.set_page_config(layout="wide")
 
 company_name = "The Best Company"
-st.title(company_name)
+st.header(company_name)
 
 description = """
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
@@ -16,3 +16,42 @@ laboris nisi ut aliquip ex ea commodo consequat.
 """
 
 st.write(description)
+
+team = "Our Team"
+st.subheader(team)
+
+# Prepare the columns
+col1, col2, col3 = st.columns(3)
+
+# Make a dataframe with the company members
+df = pandas.read_csv("data.csv")
+
+# Add content to the first column
+with col1:
+    # Iterate over only the first four rows
+    for index, row in df[:4].iterrows():
+        st.subheader(f'{row["first name"].title()} {row["last name"].title()}')
+        # Add member's role in the company
+        st.write(row["role"])
+        # Add member's photo
+        st.image("images/" + row["image"])
+
+# Add content to the second column
+with col2:
+    # Iterate over rows 4 to 7
+    for index, row in df[4:8].iterrows():
+        st.subheader(f'{row["first name"].title()} {row["last name"].title()}')
+        # Add member's role in the company
+        st.write(row["role"])
+        # Add member's photo
+        st.image("images/" + row["image"])
+
+# Add content to the third column
+with col3:
+    # Iterate over only the first four rows
+    for index, row in df[8:].iterrows():
+        st.subheader(f'{row["first name"].title()} {row["last name"].title()}')
+        # Add member's role in the company
+        st.write(row["role"])
+        # Add member's photo
+        st.image("images/" + row["image"])
